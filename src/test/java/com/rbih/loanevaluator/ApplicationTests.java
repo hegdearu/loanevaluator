d8004a4c569abe8756d7fa7aa94f3ff6df3962dd
Aravind Hegde <hegdearu23@gmail.com>
2026-03-17 18:59:02 +0530

    Initialize Spring Boot 3 project with Maven and H2 dependency.

---
+++ src/test/java/com/rbih/loanevaluator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbih.loanevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
